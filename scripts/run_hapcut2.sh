3c86923fa336b3f536b5f0b25fce99faa133ef7f
evolgenomics <frankyc@tue.mpg.de>
2021-03-19 16:12:23 +0100

    Update run_hapcut2.sh

--- scripts/run_hapcut2.sh
+++ scripts/run_hapcut2.sh
@@ -3,7 +3,7 @@ tag=$1;
 folder=$2;
 
 cd $folder;
-for i in `seq 0 2 967`; do echo "HAPCUT - $i"; ../../scripts/hapcutVcf.helera1_demo.sh hap_$i.linkedReads.diploid.$tag.bam stitch.Herato1603.450000.550000.PL.reheadered.vcf.gz $tag; done
+for i in `seq 0 2 967`; do echo "HAPCUT - $i"; ../../scripts/hapcutVcf.sh hap_$i.linkedReads.diploid.$tag.bam stitch.Herato1603.450000.550000.PL.reheadered.vcf.gz $tag; done
 bcftools merge -Oz -o stitch.Herato1603.450000.550000.PL.HAPCUT2.linked.vcf.gz `ls tmp/hap_*.$tag.PL.HAPCUT2.vcf.gz | sort -k 1,1V`; 
 bcftools merge -Oz -o stitch.Herato1603.450000.550000.PL.HAPCUT2.unlinked.vcf.gz `ls tmp/hap_*.$tag.PL.HAPCUT2_unlinked.vcf.gz | sort -k 1,1V`; 
 tabix stitch.Herato1603.450000.550000.PL.HAPCUT2.linked.vcf.gz;
